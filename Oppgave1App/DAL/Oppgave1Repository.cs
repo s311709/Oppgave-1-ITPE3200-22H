9f26f80b636068da45cff5d1490c898f3894f2d5
s311709 <piaerenmia@gmail.com>
2022-09-21 08:46:53 +0200

    lagt til Controller, Dal og Models

---
+++ Oppgave1App/DAL/Oppgave1Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oppgave1App.DAL
+{
+    public class Oppgave1Repository
+    {
+    }
+}
---
+++ Oppgave1App/Models/Oppgave1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oppgave1App.Models
+{
+    public class Oppgave1
+    {
+    }
+}
