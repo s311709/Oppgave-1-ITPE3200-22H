d334e9f587229ccdb57a0b138e11286674d9ebef
s311709 <piaerenmia@gmail.com>
2022-09-23 12:38:25 +0200

    Lagt til lazy loading

--- UFOApp/DAL/UFOContext.cs
+++ UFOApp/DAL/UFOContext.cs
@@ -8,7 +8,24 @@ using System.Threading.Tasks;
 namespace UFOApp.DAL
 {
 
+    public class EnkeltObservasjon
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Info { get; set; }
+
+        //Når denne er virtual lastes observatøren inn samtidig med EnkeltObservasjon med bruk av lazy loading
+        virtual public Observatør Observatør { get; set; }
+
+        virtual public UFO UfoObservert { get; set; }
+    }
     public class UFO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Info { get; set; }
+    }
+    public class Observatør
     {
         [Key]
         public int Id { get; set; }
@@ -26,7 +43,16 @@ namespace UFOApp.DAL
         }
 
         public DbSet<UFO> UFOer { get; set; }
+        public DbSet<EnkeltObservasjon> EnkeltObservasjoner { get; set; }
+        public DbSet<Observatør> Observatører { get; set; }
+        
+        
+        //Denne gjør det mulig å bruke lazy loading
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        optionsBuilder.UseLazyLoadingProxies();
+        }
 
     }
-
+    
 }
