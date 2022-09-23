14c62af8d724cc93bc290a3fe3c3053e2d0bfe05
s311709 <piaerenmia@gmail.com>
2022-09-23 13:14:31 +0200

    Satt opp databasestruktur

--- UFOApp/DAL/UFOContext.cs
+++ UFOApp/DAL/UFOContext.cs
@@ -12,28 +12,31 @@ namespace UFOApp.DAL
     {
         [Key]
         public int Id { get; set; }
-        public string Info { get; set; }
-
-        //Når denne er virtual lastes observatøren inn samtidig med EnkeltObservasjon med bruk av lazy loading
+        public DateTime TidspunktObservert { get; set; } //Dato+klokkeslett
+        public string KommuneObservert { get; set; }
+        public string BeskrivelseAvObservasjon { get; set; }
         virtual public Observatør Observatør { get; set; }
-
-        //Når denne er virtual lastes observatøren inn samtidig med EnkeltObservasjon med bruk av lazy loading
         virtual public UFO ObservertUFO { get; set; }
     }
     public class UFO
     {
         [Key]
         public int Id { get; set; }
-        public string Info { get; set; }
-
+        public string Modell { get; set; }
+        public string GangerObservert { get; set; }
+        public DateTime SistObservert { get; set; }
         public virtual List<EnkeltObservasjon> Observasjoner { get; set; }
     }
     public class Observatør
     {
         [Key]
         public int Id { get; set; }
-        public string Info { get; set; }
-        public virtual List<EnkeltObservasjon> ObservasjonerRegistrert { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public string Telefonnummer { get; set; }
+        public string Epost {get; set;}
+        public int AntallRegistrerteObservasjoner { get; set; }
+        public virtual List<EnkeltObservasjon> RegistrerteObservasjoner { get; set; }
 
     }
 
@@ -52,7 +55,7 @@ namespace UFOApp.DAL
         public DbSet<Observatør> Observatører { get; set; }
         
         
-        //Denne gjør det mulig å bruke lazy loading
+        //Denne gjør det mulig å bruke lazy loading (virtual atributtene)
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
         optionsBuilder.UseLazyLoadingProxies();
