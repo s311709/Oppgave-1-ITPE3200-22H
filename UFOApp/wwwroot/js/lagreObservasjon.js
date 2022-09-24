41a9797e34e4d6bb7986ee4c583c1911eb6c2289
s311709 <piaerenmia@gmail.com>
2022-09-24 14:22:49 +0200

    la inn en kommentar i lagreObservasjon

--- UFOApp/wwwroot/js/lagreObservasjon.js
+++ UFOApp/wwwroot/js/lagreObservasjon.js
@@ -1,1 +1,3 @@
-﻿
+﻿/* for å lagre dato+tid i en input er det bare å bruke noe som likner dette
+ *  <input type='datetime-local' id='dato'>
+ *  Trenger ikke parse eller noe, bare å sende rett til db /*
