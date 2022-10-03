8c7a8df0504de4babb8a89fc1b158577c2fd71f5
sarazween <s354349@oslomet.no>
2022-10-03 14:12:45 +0200

    kobler endreObservasjon.html til endreObservasjon funksjonen

--- UFOApp/wwwroot/js/endreObservasjon.js
+++ UFOApp/wwwroot/js/endreObservasjon.js
@@ -1,1 +1,21 @@
-﻿
+﻿$(document).ready(function () {
+    //henter ut UFO-objekter til dropdown list
+    $(function () {
+        $.get("UFO/HentAlleUFOer", function (UFOer) {
+            formaterUFOer(UFOer);
+        });
+    });
+
+    function formaterUFOer(UFOer) {
+        for (let UFO of UFOer) {
+            $("#inputform").append('<option>' + UFO.kallenavn + '</option>');
+        }
+    }
+});
+
+function finnModellnavn(UFOnavn) {
+    $.get("UFO/HentEnUFO?kallenavn=" + UFOnavn, function (UFO) {
+        $("#modell").val(UFO.modell);
+
+    });
+}
