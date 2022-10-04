45fd880e7af519344027fcb0873694808717f633
sarazween <s354349@oslomet.no>
2022-10-04 14:41:21 +0200

    oppretter endreObservasjon()-funksjonen, men feiler å hente kunde.id fra url

--- UFOApp/wwwroot/js/endreObservasjon.js
+++ UFOApp/wwwroot/js/endreObservasjon.js
@@ -1,21 +1,58 @@
-﻿$(document).ready(function () {
-    //henter ut UFO-objekter til dropdown list
-    $(function () {
-        $.get("UFO/HentAlleUFOer", function (UFOer) {
-            formaterUFOer(UFOer);
-        });
-    });
+﻿$(function () {
 
-    function formaterUFOer(UFOer) {
-        for (let UFO of UFOer) {
-            $("#inputform").append('<option>' + UFO.kallenavn + '</option>');
-        }
-    }
+    //hent kunden med kunde-id fra url og vis denne i skjemaet
+    const id = window.location.search.substring(1);
+    const url = "UFO/HentEnObservasjon?" + id;
+
+    $.get(url, function (UFO) {
+
+        //iden
+        $("#id").val(UFO.id);
+
+        //UFOen:
+        $("#UFOnavn").val(UFO.fornavn);
+        $("#modell").val(UFO.etternavn);
+        $("#dato").val(UFO.id);
+        $("#kommune").val(UFO.id);
+        $("#beskrivelse").val(UFO.id);
+
+        //observatør:
+        $("#fornavn").val(UFO.id);
+        $("#etternavn").val(UFO.id);
+        $("#telefon").val(UFO.id);
+        $("#epost").val(UFO.id);
+
+    });
 });
 
-function finnModellnavn(UFOnavn) {
-    $.get("UFO/HentEnUFO?kallenavn=" + UFOnavn, function (UFO) {
-        $("#modell").val(UFO.modell);
+function endreObservasjon() {
+    const observasjon = {
 
+        //iden
+        id: $("#id").val(),
+
+        //UFOen:
+        KallenavnUFO: $("#UFOnavn").val();
+        Modell: $("#modell").val();
+        TidspunktObservert: $("#dato").val();
+        KommuneObservert: $("#kommune").val();
+        BeskrivelseAvObservasjon: $("#beskrivelse").val();
+
+        //observatør:
+        FornavnObservatør: $("#fornavn").val();
+        EtternavnObservatør: $("#etternavn").val();
+        TelefonObservatør: $("#telefon").val();
+        EpostObservatør: $("#epost").val();
+
+    };
+
+    const url= "UFO/EndreObservasjon"
+    $.post(url, observasjon, function (OK) {
+        if (OK) {
+            window.location.href = 'index.html';
+        }
+        else {
+            $("#feil").html("Feil i db ved endring - prøv igjen senere")
+        }
     });
-}
+};
